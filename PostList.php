d0ea4d5789a7d7b94d74c9ace3175ebb0b9e9111
Jason Bigelow <s3606501@student.rmit.edu.au>
2019-08-31 16:15:31 +1000

    Basic post rendering without html

--- PostList.php
+++ PostList.php
@@ -1,9 +1,14 @@
 <?php
 require_once 'Component.php';
+require_once 'fbl_common.php';
 
 class PostList extends Component {
+
+    private $conn;
+
     function renderHTML() {
         $this->attr['name'] = $_SESSION['email'];
+        db_connect($this->conn);
         echo <<<EOT
 <div id='main-content-container'>
   <div class="make-post-container">
@@ -13,32 +18,77 @@ class PostList extends Component {
     <input value="Make post" type="submit"/>
     </form>
   </div>
-
-  <div class="post">
-  <p>Body</p>
-  <!-- ... n paragraphs -->
-
-    <p>
-    Bacon ipsum dolor amet pastrami hamburger boudin, frankfurter burgdoggen turducken pork cow sausage ball tip tri-tip kielbasa ground round. Venison shank bresaola rump bacon pastrami. Filet mignon flank shoulder bacon cupim corned beef shank prosciutto drumstick frankfurter pig tail chicken ribeye burgdoggen. Burgdoggen meatball beef ribs alcatra biltong pork belly.
-    </p>
-
-    <p>
-    Tri-tip biltong capicola swine bacon ham hock. Rump tongue turkey, short ribs salami buffalo picanha shankle sirloin. Kielbasa tail chuck swine. Shoulder chuck porchetta corned beef pastrami. Turducken filet mignon short ribs leberkas, drumstick capicola pancetta.
-    </p>
-
-    <p>
-    Corned beef fatback strip steak tri-tip ball tip meatloaf jowl venison. Beef ribs burgdoggen meatball, sirloin venison tongue buffalo alcatra boudin andouille pork loin. Ham hock kevin buffalo picanha, andouille capicola tongue sirloin filet mignon meatball chicken venison boudin pig ground round. Kielbasa ribeye ham, brisket pork belly drumstick porchetta cow swine.
-    </p>
-    <p>
-    Ham hock spare ribs ham kevin andouille, cupim porchetta drumstick buffalo beef ball tip doner sausage capicola prosciutto. Spare ribs salami tenderloin, ham hock ball tip landjaeger picanha meatloaf pig hamburger chicken cupim andouille. Jowl pig turducken t-bone swine pastrami, andouille venison. Cupim meatball ball tip bacon kevin turducken, andouille meatloaf doner brisket porchetta pork chop. Short ribs rump boudin pastrami drumstick buffalo jerky.
-    <p>
-    Kielbasa tenderloin venison shoulder kevin jowl drumstick tri-tip strip steak. Ham frankfurter ham hock beef ribs capicola. Capicola ground round ball tip boudin shankle shoulder. Pig shankle burgdoggen, short loin alcatra leberkas meatloaf ground round kielbasa tri-tip pork chop. Leberkas tenderloin tongue brisket short loin. Pork shank pork chop ball tip, pancetta meatloaf landjaeger. Landjaeger brisket jerky, ham leberkas flank hamburger picanha beef ribs ham hock.
-    </p>
-      <!-- ... n posts -->
-    </div>
-</div>
-
 EOT;
 
+        $this->renderPosts();
+        oci_close($this->conn);
+    }
+
+    function renderPosts() {
+        /* Retrieve the root posts of this user - ones which are not a reply */
+        $queryStr = "SELECT POST_ID FROM POST WHERE PARENT_POST_ID IS NULL AND
+            POSTER_EMAIL_ADDRESS = :email";
+        $stmt = oci_parse($this->conn, $queryStr);
+        $email = $_SESSION['email'];
+        oci_bind_by_name($stmt, 'email', $email);
+        $succ = oci_execute($stmt);
+
+        if (!$succ) {
+            echo '<div class="post"><p>Error retrieving your posts</p></div>';
+            oci_close($conn);
+        } else {
+            echo "Success retrieving posts. Number of results: ",
+                 oci_num_rows($stmt);
+        }
+
+        echo "Your posts: [";
+        while ($row = oci_fetch_row($stmt)) {
+            $this->renderPost($row[0]);
+        }
+        echo "]";
+        oci_free_statement($stmt);
+    }
+
+    function renderPost($postId) {
+        /* Select all posts under this with a hierarchical query then render
+           them recursively */
+
+        $queryStr = "select post_id, body, posted, screen_name, parent_post_id, l
+            from (
+                select post_id, body, posted, poster_email_address,
+                parent_post_id, level as l from post
+                connect by prior post_id = parent_post_id
+                start with post_id = :post_id
+            ) join member on member.email_address = poster_email_address";
+
+        $stmt = oci_parse($this->conn, $queryStr);
+        oci_bind_by_name($stmt, 'post_id', $postId);
+        $succ = oci_execute($stmt);
+
+        if (!$succ) {
+            echo "Couldn't retrieve post: ", $postId;
+        }
+
+        $level = 0;
+        while ($row = oci_fetch_assoc($stmt)) {
+            //var_dump($row);
+            echo '<p>',$row['POST_ID'], $row['BODY']->load(), $row['POSTED'],
+                 $row['SCREEN_NAME'], $row['PARENT_POST_ID'], $row['L'],'</p>';
+        }
+
+
+        /*
+        echo '<div class="post">
+            <span class="post-id">#</span>
+            */
+        echo $postId, ",";
     }
 }
+/*
+  <div class="post">
+  <p>Body</p>
+  <!-- ... n posts -->
+  </div>
+</div>
+EOT;
+*/
