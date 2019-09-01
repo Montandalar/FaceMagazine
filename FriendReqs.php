49965e45976690ec3bf0ac4986eb26ef908930e1
Jason Bigelow <s3606501@student.rmit.edu.au>
2019-09-01 12:50:23 +1000

    Database query for incoming requests

--- FriendReqs.php
+++ FriendReqs.php
@@ -3,23 +3,44 @@ require_once 'Component.php';
 
 class FriendReqs extends Component {
     function renderHTML() {
-        echo <<<EOT
-<div id="manage-friends">
-    <div id="manage-friend-reqs">
-        <div class="friend-req">
-        </div>
-        <!--- ... n requests -->
-    </div>
-
-    <div id="add-friends">
-        <form action="search_people.php">
-        <input type="text" name="person" placeholder="Search people..."/>
-        <input value="Search" type="submit"/>
-        </form>
-    </div>
-</div>
-
-EOT;
+        $this->renderRequests();
+        $this->renderSearchPane();
     }
 
+    function renderRequests() {
+        echo '<div id="manage-friend-reqs">';
+        db_connect($conn);
+        $queryStr = "select screen_name from (
+                        select member1 from friendship
+                        where accepted is null and
+                        member2 = :us)
+                    join MEMBER on MEMBER1 = EMAIL_ADDRESS";
+        $stmt = oci_parse($conn, $queryStr);
+        oci_bind_by_name($stmt, "us", $_SESSION['email']);
+        $succ = oci_execute($stmt);
+        if (!$succ) {
+            echo "<span>Error retrieving friend requests</span></div>";
+            return;
+        }
+        while ($row = oci_fetch_row($stmt)) {
+            $this->renderRequest($row);
+        }
+        echo '</div>';
+    }
+
+    function renderRequest($row) {
+        echo $row[0], ", ";
+    }
+
+    function renderSearchPane() {
+        ?>
+       <div id="add-friends">
+            <form action="search_people.php">
+            <input type="text" name="person" placeholder="Search people..."/>
+            <input value="Search" type="submit"/>
+            </form>
+        </div>
+    </div>
+    <?php
+    }
 }
