502981bb5e69f8d615e5e50d1fd40c02d7e57529
Jason Bigelow <s3606501@student.rmit.edu.au>
2019-10-01 09:16:14 +0000

    Profile update with mongo

--- update_profile.php
+++ update_profile.php
@@ -4,30 +4,18 @@ require_once 'fbl_common.php';
 
 class ProfileUpdate extends AuthenticatedPage {
     function doUpdate() {
-        db_connect($conn);
-        $stmt = oci_parse($conn,
-                'UPDATE Member
-                SET screen_name = :scr, location = :loc, status = :stat,
-                visibility = :vis
-                WHERE email_address = :email'
-                );
+        db_connect($client);
+        $collection = $client->fbl->Members;
+        $result = $collection->updateOne(['_id' => $_SESSION['email']],
+                ['$set' => ['visibility' => $_POST['vis'],
+                'location' => $_POST['loc'],
+                'status' => $_POST['stat'],
+                'screen_name' => $_POST['scr']]
+                ]);
 
-        $email = $_SESSION['email'];
-        $loc = $_POST['loc'];
-        $stat = $_POST['stat'];
-        $scr = $_POST['scr'];
-        $vis = $_POST['vis'];
-
-        oci_bind_by_name($stmt, "loc", $loc);
-        oci_bind_by_name($stmt, "stat", $stat);
-        oci_bind_by_name($stmt, "scr", $scr);
-        oci_bind_by_name($stmt, "vis", $vis);
-        oci_bind_by_name($stmt, "email", $email);
-
-        $ret = oci_execute($stmt);
-        oci_close($conn);
-        return $ret;
-
+        // If no exception happened, either the profile was already correct or
+        // has been updately appropriately.
+        return true;
     }
     
     function pageMain() {
