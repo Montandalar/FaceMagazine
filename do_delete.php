900e98fdf848be24050ec2dfae9e1071652c97d9
Jason Bigelow <s3606501@student.rmit.edu.au>
2019-10-01 08:43:51 +0000

    Delete account for Mongo

--- do_delete.php
+++ do_delete.php
@@ -4,13 +4,14 @@ require_once 'fbl_common.php';
 
 class DeleteAction extends AuthenticatedPage {
     function doDelete() {
-        db_connect($conn);
-        $stmt = oci_parse($conn, 'DELETE FROM member where email_address = :email');
         $email = $_SESSION['email'];
-        oci_bind_by_name($stmt, "email", $email);
-        $succ = oci_execute($stmt);
-        oci_close($conn);
-        if ($succ) {
+
+        db_connect($client);
+        $collection = $client->fbl->Members;
+        $result = $collection->
+            deleteOne(['_id' => $_SESSION['email']]);
+
+        if ($result->getDeletedCount() > 0) {
             session_destroy();
             echo "Your account is gone forever";
         } else {
