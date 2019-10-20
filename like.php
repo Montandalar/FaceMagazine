e8009314e1a04fb5ffdd7561cc434f46d56f0c7d
Jason Bigelow <s3606501@student.rmit.edu.au>
2019-10-20 04:30:08 +0000

    Like/unlike code for mongo

--- like.php
+++ like.php
@@ -7,33 +7,20 @@ $authResult = $auth->do_login();
 
 session_start();
 
-$likeTarget = $_POST["like_target"];
+db_connect($client);
+$collection = $client->fbl->Posts;
 
-db_connect($conn);
-
-$queryStr = "";
+$operator = '$addToSet';
 if (isset($_POST["unlike"])) {
-    $queryStr =
-        "delete from likes
-        where member_email_address = :email
-        and post_id = :postid";
-}
-else {
-    $queryStr = 
-        "insert into likes(member_email_address, post_id)
-        values (:email, :postid)";
-}
-$stmt = oci_parse($conn, $queryStr);
+    $operator = '$pull';
+}   
+$result = $collection->updateOne(
+    ["_id" => (new MongoDB\BSON\ObjectId($_POST['like_target']))],
+    [$operator => ['liked' => $_SESSION['email']]]
+);
 
-oci_bind_by_name($stmt, 'email', $_SESSION['email']);
-oci_bind_by_name($stmt, 'postid', $likeTarget);
+// If something exceptional happens, Mongo will throw an exception.
 
-$succ = oci_execute($stmt);
-
-if (!$succ) {
-    echo "There was an error and we could not process your like";
-    exit(1);
-}
 header("http/1.1 303 see other");
 header("location: home.php#postno$likeTarget");
 ?>
